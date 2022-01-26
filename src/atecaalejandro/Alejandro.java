2a6e86d46dd94ac481304c1d354e4b846f128c25
dam <dam@203P11>
2022-01-26 10:10:06 +0100

    crea rema pruebas

---
+++ src/atecaalejandro/Alejandro.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atecaalejandro;
+
+/**
+ *
+ * @author dam
+ */
+public class Alejandro {
+    String nombre;
+    int edad;
+}
